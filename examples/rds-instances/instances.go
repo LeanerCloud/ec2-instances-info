cc2816fc8061735577a814cd3a1eca40601e4277
Cristian Magherusan-Stanciu <cristi@leanercloud.com>
2023-08-31 10:44:52 +0200

    Print more fields in the RDS example

--- examples/rds-instances/instances.go
+++ examples/rds-instances/instances.go
@@ -23,13 +23,42 @@ func main() {
 		if i.InstanceType == instanceType {
 			if pricing, found := i.Pricing[region]; found {
 				fmt.Println("Instance type:", i.InstanceType)
-				fmt.Println("CPU:", i.PhysicalProcessor)
 				fmt.Println("Arch:", i.Arch)
-				// Print other fields as needed
+				fmt.Println("Clock Speed:", i.ClockSpeed)
+				fmt.Println("Current Generation:", i.CurrentGeneration)
+				fmt.Println("Dedicated EBS Throughput:", i.DedicatedEbsThroughput)
+				fmt.Println("EBS Baseline Bandwidth:", i.EbsBaselineBandwidth)
+				fmt.Println("EBS Baseline IOPS:", i.EbsBaselineIops)
+				fmt.Println("EBS Baseline Throughput:", i.EbsBaselineThroughput)
+				fmt.Println("EBS IOPS:", i.EbsIops)
+				fmt.Println("EBS Max Bandwidth:", i.EbsMaxBandwidth)
+				fmt.Println("EBS Optimized:", i.EbsOptimized)
+				fmt.Println("EBS Optimized By Default:", i.EbsOptimizedByDefault)
+				fmt.Println("EBS Throughput:", i.EbsThroughput)
+				fmt.Println("Enhanced Networking Supported:", i.EnhancedNetworkingSupported)
+				fmt.Println("Family:", i.Family)
+				fmt.Println("Instance Family:", i.InstanceFamily)
+				fmt.Println("Instance Type Family:", i.InstanceTypeFamily)
+				fmt.Println("Memory:", i.Memory)
+				fmt.Println("Network Performance:", i.NetworkPerformance)
+				fmt.Println("Physical Processor:", i.PhysicalProcessor)
+				fmt.Println("Pretty Name:", i.PrettyName)
 
+				// Print Pricing information for different database engines
 				fmt.Println("Aurora PostgreSQL OD cost:", pricing.AuroraPostgreSQL.OnDemand)
 				fmt.Println("PostgreSQL OD cost:", pricing.PostgreSQL.OnDemand)
-				// Print other pricing fields for different database engines
+				// ... and so on for other database engines
+
+				fmt.Println("Processor Architecture:", i.ProcessorArchitecture)
+				fmt.Println("Region Code:", i.RegionCode)
+				// Print Regions information if needed
+				// fmt.Println("Regions:", i.Regions)
+				fmt.Println("Service Code:", i.Servicecode)
+				fmt.Println("Service Name:", i.Servicename)
+				fmt.Println("Storage:", i.Storage)
+				fmt.Println("vCPU:", i.Vcpu)
+
+				// Add more fields and pricing data as needed
 
 				break // Stop iterating after finding the desired instance type
 			}
